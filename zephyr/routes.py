615bf7e69f11f959a78be05a566dcb766ea39387
Oyematth01 <oyematth@gmail.com>
2024-09-03 01:49:24 +0100

    zephyr

--- zephyr/routes.py
+++ zephyr/routes.py
@@ -1,11 +1,17 @@
-from market import app
+from zephyr import app
 from flask import render_template
-from market.models import Item
+# from zephyr.models import Item
 
 @app.route('/')
 @app.route('/home')
 def home_page():
-    return render_template('home.html')
+     social_links ={
+        "twitter": "https://x.com/oyematth",
+        "linkedin": "https://www.linkedin.com/in/matthew-oyelami-1223b5244/",
+        "github": "https://github.com/Oyematth01",
+        "instagram": "https://www.instagram.com/oyematth/",
+        "medium": "https://medium.com/@oyematth"}
+     return render_template('home.html', social_links=social_links)
 
 # @app.route('/zephyr')
 # def market_page():
