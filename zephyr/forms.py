4e1387d4959819ca7302f29425290d4ff6d09534
Oyematth01 <oyematth@gmail.com>
2024-09-06 06:35:56 +0100

    zephr

--- zephyr/forms.py
+++ zephyr/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField, IntegerField, DateField, EmailField, SelectField, SubmitField 
-from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError, NumberRange
 
 class RegisterForm(FlaskForm):
     name = StringField(label='Full Name', validator=[DataRequired()])
@@ -18,9 +18,10 @@ class LoginForm(FlaskForm):
 
 
 class BookingForm(FlaskForm):
-    checkin = IntegerField(label='Arrival Date', validators=[DataRequired()])
-    checkout = IntegerField(label='Departure Date', validators=[DataRequired()])
-    Adults = StringField(Label='Adults', validators=[DataRequired()])
+    checkin = DateField(label='Arrival Date', validators=[DataRequired()])
+    checkout = DateField(label='Departure Date', validators=[DataRequired()])
+    Adults = IntegerField(Label='Adults', validators=[DataRequired(), NumberRange(min=0, max=6)])
+    children = IntegerField(label='Children', validators=[DataRequired(), NumberRange(min=0, max=6)])
     room_type = SelectField(label='Room Type', choices=[('Single Room', 'Single Room'), ('Single Deluxe Room', 'Single Deluxe Room'), 
                                                         ('Double Deluxe Room', 'Double Deluxe Room'), 
                                                         ('Honeymoon Suite', 'Honeymoon Suite'), 
@@ -30,3 +31,8 @@ class BookingForm(FlaskForm):
                                                         ('Queen Room', 'Queen Room'), ('Duplex Room', 'Duplex Room')]
                             )
     book_now = SubmitField("Book Now!")
+
+class ConfirmBookingForm(FlaskForm):
+    name = StringField(label='Enter your Full Name', validators=[DataRequired(), Length(min=5, max=60)])
+    Email = EmailField(label='Email Address:', validators=[Email(), DataRequired()])
+    Occupation = StringField(label=' Enter your Occupation', validators=[DataRequired()])
--- zephyr/routes.py
+++ zephyr/routes.py
@@ -1,7 +1,7 @@
-om zephyr import app
+from zephyr import app
 from flask import render_template, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import SubmitField
+from wtforms.fields import SubmitField
 
 
 @app.route('/')
@@ -17,14 +17,30 @@ def home_page():
     form = BookingForm()
     if form.validate_on_submit():
         # Redirect to a new page after the form is submitted
-        return redirect(url_for('booking_page'))
-    return render_template('home.html', social_links=social_links, form=form)
+        return redirect(url_for('confirm_booking', name=form.name.data, email=form.email.data, room_number=form.room_number.data))
+    return render_template('home.html', confirm_booking.html, social_links=social_links, form=form)
 
 @app.route('/booking')
 def booking_page():
     return "Welcome to the booking page!"
 
+
+@app.route('/confirm_booking', methods=['GET', 'POST'])
+def confirm_booking():
+    # Initialize ConfirmBookingForm with data from the URL parameters
+    form = ConfirmBookingForm(request.args)
+    if form.validate_on_submit():
+        # Process the confirmed booking here
+        return redirect(url_for('booking_complete'))
+    return render_template('confirm_booking.html', form=form)
+
+@app.route('/booking_complete')
+def booking_complete():
+    return "Booking Confirmed!"
+
 # @app.route('/zephyr')
 # def market_page():
 #     items = Item.query.all()
 #     return render_template('market.html', items=items)
+
+
