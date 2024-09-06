f3d610a71e6cedf71a03f3eb7d9a8ef702a18521
Oyematth01 <oyematth@gmail.com>
2024-09-06 03:31:15 +0100

    zephyr

--- zephyr/forms.py
+++ zephyr/forms.py
@@ -1,11 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms.fields import StringField, PasswordField, SubmitField, IntegerField, DateField, EmailField, SelectField, SubmitField 
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
 
 class RegisterForm(FlaskForm):
+    name = StringField(label='Full Name', validator=[DataRequired()])
     username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
     email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
-    password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
-    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
+    password = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
+    confirm_password = PasswordField(label='Confirm Password:', validators=[EqualTo('password'), DataRequired()])
     submit = SubmitField(label='Create Account')
 
 
@@ -13,3 +15,17 @@ class LoginForm(FlaskForm):
     username = StringField(label='User Name:', validators=[DataRequired()])
     password = PasswordField(label='Password:', validators=[DataRequired()])
     submit = SubmitField(label='Sign in')
+
+
+class BookingForm(FlaskForm):
+    checkin = IntegerField(label='Arrival Date', validators=[DataRequired()])
+    checkout = IntegerField(label='Departure Date', validators=[DataRequired()])
+    Adults = StringField(Label='Adults', validators=[DataRequired()])
+    room_type = SelectField(label='Room Type', choices=[('Single Room', 'Single Room'), ('Single Deluxe Room', 'Single Deluxe Room'), 
+                                                        ('Double Deluxe Room', 'Double Deluxe Room'), 
+                                                        ('Honeymoon Suite', 'Honeymoon Suite'), 
+                                                        ('Villa Room', 'Villa Room'), ('Executive Room', 'Executive Room'), 
+                                                        ('Triple Room', 'Triple Room'), ('Bridal Suite', 'Bridal Suite'), 
+                                                        ('Standard Suite Room', 'Standard Suite Room'), ('King Room', 'King Room'), 
+                                                        ('Queen Room', 'Queen Room'), ('Duplex Room', 'Duplex Room')]
+                            )
